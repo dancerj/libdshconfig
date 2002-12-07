acda8ca1a3e427d326b8f4e01c87b6ebf308dcdb
dancer <dancer>
2002-12-07 09:25:24 +0000

    +	* autogen.sh: Copied from dlisp, to allow bootstrapping with +	automake1.7 and autoconf2.5

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# autogenerate script
 
-aclocal && libtoolize -c --force && automake --foreign -a -c && autoconf && autoheader
+libtoolize -f -c && aclocal && autoheader && automake --foreign -a -c && autoconf
 
